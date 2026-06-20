6f9c905aad81a6bd42c10eecbc8e489f14cdd294
Cecilia Camargo <ceciliazica@MacBook-Air-de-Cecilia.local>
2026-06-20 15:18:00 -0300

    reescreve disparo e rocket_thread para usar pool; hitbox 3x3

--- src/input.c
+++ src/input.c
@@ -1,60 +1,50 @@
+#include <stdlib.h>
 #include <ncurses.h>
+#include <pthread.h>
 #include "../include/common.h"
+#include "../include/entities.h"
 #include "../include/input.h"
 
-void* input_thread_fn(void* arg) {
-    (void)arg;
-    int ch;
+void process_input(int ch) {
+    pthread_mutex_lock(&game.state_mutex);
 
-    while (1) {
-        pthread_mutex_lock(&game.state_mutex);
-        if (game.game_over) {
-            pthread_mutex_unlock(&game.state_mutex);
-            break;
-        }
-        pthread_mutex_unlock(&game.state_mutex);
-
-        ch = getch();
-
-        if (ch == ERR) {
-            continue;
-        }
-
-        pthread_mutex_lock(&game.state_mutex);
-
-        if (ch == KEY_LEFT) {
-            if (game.current_angle > ANGLE_HORIZ_LEFT) {
-                game.current_angle--;
+    if (ch == KEY_LEFT) {
+        if (game.current_angle > ANGLE_HORIZ_LEFT)
+            game.current_angle--;
+    } else if (ch == KEY_RIGHT) {
+        if (game.current_angle < ANGLE_HORIZ_RIGHT)
+            game.current_angle++;
+    } else if (ch == ' ') {
+        if (game.rockets_in_battery > 0) {
+            int rocket_idx = -1;
+            for (int i = 0; i < MAX_ROCKETS; i++) {
+                if (!game.pool_rockets[i].active) {
+                    rocket_idx = i;
+                    break;
+                }
             }
-        } else if (ch == KEY_RIGHT) {
-            if (game.current_angle < ANGLE_HORIZ_RIGHT) {
-                game.current_angle++;
-            }
-        } else if (ch == ' ') {
-            if (game.rockets_in_battery > 0) {
+            if (rocket_idx != -1) {
                 game.rockets_in_battery--;
-                
-                // aloca espaço para passar as coordenadas iniciais para a thread
-                Rocket* novo_foguete = malloc(sizeof(Rocket));
-                novo_foguete->pos.x = SCREEN_WIDTH / 2;
-                novo_foguete->pos.y = SCREEN_HEIGHT - 3;
-                novo_foguete->trajectory = game.current_angle;
-                
-                pthread_t rocket_tid;
-                if (pthread_create(&rocket_tid, NULL, rocket_thread_fn, novo_foguete) == 0) {
-                    pthread_detach(rocket_tid); 
+                game.pool_rockets[rocket_idx].pos.x      = SCREEN_WIDTH / 2;
+                game.pool_rockets[rocket_idx].pos.y      = SCREEN_HEIGHT - 3;
+                game.pool_rockets[rocket_idx].trajectory = game.current_angle;
+                game.pool_rockets[rocket_idx].active     = true;
+
+                int *idx_arg = malloc(sizeof(int));
+                *idx_arg = rocket_idx;
+                if (pthread_create(&game.pool_rockets[rocket_idx].thread_id,
+                                   NULL, rocket_thread_fn, idx_arg) == 0) {
+                    game.pool_rockets[rocket_idx].thread_started = true;
                 } else {
-                    free(novo_foguete);
+                    free(idx_arg);
+                    game.pool_rockets[rocket_idx].active = false;
+                    game.rockets_in_battery++;
                 }
             }
         }
-            }
-        } else if (ch == 'q' || ch == 'Q') {
-            game.game_over = true;
-        }
-
-        pthread_mutex_unlock(&game.state_mutex);
+    } else if (ch == 'q' || ch == 'Q') {
+        game.game_over = true;
     }
 
-    return NULL;
+    pthread_mutex_unlock(&game.state_mutex);
 }
