70fac53d7cc00aed1ae61e90a418c8c1c917e5fa
Cecilia Camargo <ceciliazica@MacBook-Air-de-Cecilia.local>
2026-06-20 15:20:20 -0300

    remove thread de input; getch no loop principal; renderiza foguetes e mira

--- src/render.c
+++ src/render.c
@@ -8,13 +8,13 @@ void init_render(void) {
     noecho();
     keypad(stdscr, TRUE);
     curs_set(0);
-    timeout(0);
+    timeout(33);
 }
 
 void draw_game(void) {
     pthread_mutex_lock(&game.state_mutex);
 
-    clear();
+    erase();
 
     for (int i = 0; i < SCREEN_WIDTH; i++) {
         mvaddch(0, i, '#');
@@ -23,11 +23,23 @@ void draw_game(void) {
     for (int i = 0; i < SCREEN_HEIGHT; i++) {
         mvaddch(i, 0, '#');
         mvaddch(i, SCREEN_WIDTH - 1, '#');
+
+    
     }
 
-    mvprintw(1, 2, "Foguetes na Bateria: %d / %d", game.rockets_in_battery, game.max_launchers);
-    mvprintw(2, 2, "Naves Abatidas: %d / %d", game.aliens_destroyed, game.total_aliens);
-    mvprintw(3, 2, "Naves Escaparam: %d", game.aliens_escaped);
+    int win_thresh = (game.total_aliens + 1) / 2;
+    int lose_thresh =
+    game.total_aliens / 2 + 1;
+    
+    mvprintw(1, 2, "Foguetes: %d/%d  |  Abatidas: %d/%d  |  Escapadas: %d/%d",
+    
+    game.rockets_in_battery,
+    game.max_launchers,
+
+    game.aliens_destroyed,
+    win_thresh,
+    game.aliens_escaped,
+    lose_thresh) ;
 
     for (int i = 0; i < MAX_ALIENS_CONCURRENT; i++) {
         if (game.pool_aliens[i].active) {
@@ -43,6 +55,23 @@ void draw_game(void) {
     else if (game.current_angle == ANGLE_HORIZ_RIGHT) angle_char = '_';
 
     mvaddch(SCREEN_HEIGHT - 2, SCREEN_WIDTH / 2, angle_char);
+
+    int ax = SCREEN_WIDTH / 2; 
+    int ay = SCREEN_HEIGHT - 2;
+    for (int step = 1; step <= 8; step++) {
+    if      (game.current_angle == ANGLE_VERTICAL)    { ay--; }
+    else if (game.current_angle == ANGLE_DIAG_LEFT)   { ay--; ax--; }
+    else if (game.current_angle == ANGLE_DIAG_RIGHT)  { ay--; ax++; }
+    else if (game.current_angle == ANGLE_HORIZ_LEFT)  { ax--; }
+    else if (game.current_angle == ANGLE_HORIZ_RIGHT) { ax++; }
+    if (ay > 0 && ax > 0 && ax < SCREEN_WIDTH - 1)
+        mvaddch(ay, ax, '.');
+}
+     for (int i = 0; i < MAX_ROCKETS; i++) {
+        if (game.pool_rockets[i].active) {
+            mvaddch(game.pool_rockets[i].pos.y, game.pool_rockets[i].pos.x, '*');
+        }
+    }
 
     refresh();
 
